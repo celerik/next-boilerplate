9495dbde63421b09c29f3917406a2771f68205b5
Diego Cardona <dcardona@celerik.com>
2021-06-17 08:15:25 -0500

    next-boilerplate

---
+++ source/src/pages/dashboard/index.js
@@ -0,0 +1,35 @@
+// @packages
+import React from 'react';
+import DashboardMenuMapper from '../../components/organisms/dashboard-menu-mapper';
+
+// @scripts
+import TemplateDashboard from '../../components/templates/dashboard';
+import { MainMenuProvider, useMainMenuContext } from '../../providers/main-menu';
+import { useExpandMainMenu } from '../../providers/main-menu/actions';
+
+// eslint-disable-next-line react/prop-types
+const Dashboard = ({ children }) => {
+    const { expanded, visible } = useMainMenuContext();
+    const expandMainMenu = useExpandMainMenu();
+
+    return (
+        <TemplateDashboard
+            isMenuExpanded={expanded}
+            isMenuVisible={visible}
+            setMenuExpanded={expandMainMenu}
+        >
+            {children}
+        </TemplateDashboard>
+    );
+};
+
+const DashboardPage = () => (
+    <MainMenuProvider>
+        <Dashboard>
+            <h1>Dashboard Router</h1>
+            <DashboardMenuMapper />
+        </Dashboard>
+    </MainMenuProvider>
+);
+
+export default DashboardPage;
